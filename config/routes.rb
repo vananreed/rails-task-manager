e4c5913eb648f49e37f7168ee12bd4ec124ae7f9
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-07-27 12:39:21 +0800

    link added between index and show page. Show page html finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
   get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/new', to: 'tasks#new'
 end
