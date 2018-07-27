11523c1816010bf999d28a3bdc7c683b6706b35d
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-07-27 14:30:33 +0800

    Add, Edit and Delete functions added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,11 +4,39 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(user_params)
+    redirect_to root_path
+  end
+
   def show
     @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
+  end
+
+  def create
+    @task = Task.new(user_params)
+    @task.save
+    redirect_to root_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
